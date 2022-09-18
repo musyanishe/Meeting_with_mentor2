986c2dce8b6515d9ddfd3d3e9453348327640681
Evgeniia Shevtsova <shevtsova-evgeniia@yandex.ru>
2022-09-18 18:42:15 +0300

    Initial Commit

---
+++ Meeting_with_mentor2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meeting_with_mentor2
+//
+//  Created by Евгения Шевцова on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
